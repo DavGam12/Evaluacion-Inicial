b8a68d2057f46cb548982929f7d370baceb3ce5a
David <154616047+DavGam12@users.noreply.github.com>
2024-09-19 20:02:16 +0200

    Changed some html and css lines. Tried to filter the icons by category.

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 let map // mapX = y; mapY = x
+let categoriesIconsData
+const categories =
+[
+    "drought",
+    "dustHaze",
+    "earthquakes",
+    "floods",
+    "landslides",
+    "manmade",
+    "seaLakeIce",
+    "severeStorms",
+    "snow",
+    "tempExtremes",
+    "volcanoes",
+    "waterColor",
+    "wildfires"   
+]
+
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM content loaded")
 
 
-    map = L.map('map').setView([42, 1], 10);
+    map = L.map('map').setView([41.669841685814625, -0.8788074741911481], 10);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('Fuck you ordinary old bitch!')
-        .openPopup();
-
-
-    const imageUrl = "/Images/Logo.png",
-    imageBounds = [[42.009, 1], [42, -3]];
-    L.imageOverlay(imageUrl, imageBounds).addTo(map);
-
-    const circle = L.circle([42, 1], {
-    color: 'maroon',
-        fillColor: '#ff3',
-        fillOpacity: 0.5,
-        radius: 1000
-    }).addTo(map);
+    L.marker([41.669841685814625, -0.8788074741911481]).addTo(map).bindPopup('San Valero Is Here')
 
     
     fetchData()
@@ -35,15 +38,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const categoriesURL = "https://eonet.gsfc.nasa.gov/api/v3/categories"
 const eventURL = "https://eonet.gsfc.nasa.gov/api/v3/events"
+const categoriesIconsURL = `${window.location.origin}/icons.json`
 
 const fetchData = async() => {
-    const [ categoriesRes, eventsRes ] = await Promise.all([fetch(categoriesURL), fetch(eventURL)])
+    const [ categoriesRes, eventsRes, categoriesIconsRes ] = await Promise.all([fetch(categoriesURL), fetch(eventURL), fetch(categoriesIconsURL)])
 
     const categoriesData = await categoriesRes.json()
     const eventsData = await eventsRes.json()
+    categoriesIconsData = await categoriesIconsRes.json()
 
-    console.log('categoriesData -->', categoriesData)
-    console.log('eventsData -->', eventsData)
+    console.log('categoriesData -->', categoriesData);
+    console.log('eventsData -->', eventsData);
+    console.log('categoriesIconsData -->', categoriesIconsData);
 
     categoriesFetch(categoriesData)
     eventsFetch(eventsData)
@@ -54,79 +60,140 @@ const categoriesFetch = async(data) => {
         const categoriesUl = document.getElementsByClassName("categories")[0]
         const categoryLi = categoriesUl.appendChild(document.createElement("li"))
         categoryLi.textContent = e.title
+        categoryLi.addEventListener("click", () => {
+            Array.from(document.getElementsByTagName('img')).forEach(i => {
+                if (i.alt.includes('mapIcon'))
+                {
+                    if (i.src.includes(categoriesIconsData[e.id])) {i.style.display = 'block'}
+                    else {i.style.display = 'none'}
+                /*if (i.alt.includes('mapIcon'))
+                    {
+                        categories.forEach(cat => {
+                            if (cat.includes(e.id))
+                                {
+                                    i.style.display = 'block'
+                                }
+                            else
+                                {
+                                    i.style.display = 'none'
+                                }
+                        })
+                    }*/
+                }
+            })
+        })
     })
 }
 
 const eventsFetch = async(data) => {
     Array.from(data.events).forEach(e => {
         console.log(e);
-        console.log(e.geometry[0].coordinates);
 
+        const iconSize = [15, 15]
         const icons =
         [
             L.icon({
                 iconUrl: '/Images/drought.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/dustHaze.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/earthquakes.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/floods.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/landslides.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/manmade.png', /* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/seaLakeIce.png',
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/severeStorms.png',
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/snow.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/tempExtremes.png',/* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/volcanoes.png',
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/waterColor.png', /* */
-                iconSize: [30, 30]
+                iconSize: iconSize
             }),
             L.icon({
                 iconUrl: '/Images/wildfires.png',
-                iconSize: [30, 30]
+                iconSize: iconSize
             })
         ]
+
+        const mainBox = document.getElementsByClassName("event-info")[0].appendChild(document.createElement("ul"))
+        mainBox.classList.add('general-box')
+        mainBox.classList.add("hide")
+
+        const category = mainBox.appendChild(document.createElement('li'))
+        category.appendChild(document.createElement('h3')).textContent = 'Category'
+        category.appendChild(document.createElement('p')).textContent = e.categories[0].title
+        
+        const title = mainBox.appendChild(document.createElement("li"))
+        title.appendChild(document.createElement('h3')).textContent = 'Title'
+        title.appendChild(document.createElement('p')).textContent = e.title
+
+        const geometryDate = e.geometry[0].date.split("T")
+        const date = mainBox.appendChild(document.createElement("li"))
+        date.appendChild(document.createElement('h3')).textContent = 'Date'
+        date.appendChild(document.createElement('p')).textContent = `${geometryDate[0]}  ${geometryDate[1].replace("Z", "")}`
+
+        if (e.description != null)
+            {
+                const description = mainBox.appendChild(document.createElement("li"))
+                description.appendChild(document.createElement('h3')).textContent = 'Description'
+                description.appendChild(document.createElement('p')).textContent = e.description
+            }
+
+        const url = mainBox.appendChild(document.createElement("li"))
+        url.appendChild(document.createElement('h3')).textContent = 'Source'
+        const a = url.appendChild(document.createElement('p'))
+        a.appendChild(document.createElement('a')).textContent = e.sources[0].url
+        a.childNodes[0].href = e.sources[0].url
+
+
         icons.forEach(i => {
+
             const iconUrlId = i.options.iconUrl.split('/')[2].split('.')
             if (iconUrlId[0].includes(e.categories[0].id))
             {
-                console.log('sus')
-                L.marker([e.geometry[0].coordinates[1], e.geometry[0].coordinates[0]], {icon: i}).addTo(map)
+                const iconMarker = L.marker([e.geometry[0].coordinates[1], e.geometry[0].coordinates[0]], {icon: i}).addTo(map)
                 .bindPopup(`[${e.geometry[0].coordinates[1]},${e.geometry[0].coordinates[0]}]`)
-                .addEventListener("click", () => {})
+                .addEventListener("click", () => {
+                    Array.from(document.getElementsByClassName("show")).forEach(shown => {
+                        shown.classList.remove("show")
+                        shown.classList.add("hide")
+                    })
+                    mainBox.classList.remove("hide")
+                    mainBox.classList.add("show")
+                })
+                iconMarker._icon.alt = 'mapIcon'
             }
         })
-
     })
 }
